c4a18578314fe71653b9b081df1bddfe70445282
Limstar12 <phji0326@gmail.com>
2022-03-30 17:40:40 +0900

    워크벤치 연결

--- api/preinterview.js
+++ api/preinterview.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var dbconfig = require('../db/dbconfig');
+var ccc = mysql.createConnection(dbconfig);
 
 router.get('/', (req,res,next) => {
-    res.send('라우터 처리')
+    ccc.query('SELECT * FROM reactinterview.interview', (error, result) =>{
+        if(error) throw error;
+        res.send(result)
+        })
 })
 
 module.exports=router;
+
+
+
+
+
+
